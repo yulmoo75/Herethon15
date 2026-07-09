50d21337003b975884143c404bb0b302f9dc9579
yulmoo75 <yulmoo0705@gmail.com>
2026-07-09 20:19:31 +0900

    Initial commit: 앱 구조 설정

--- Herethon_prj/settings.py
+++ Herethon_prj/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
+    'posts',
+    'records',
+    'support',
+    'users',
 ]
 
 MIDDLEWARE = [
